0c85db60f8139d4569532926e5e4114077c20f31
Harpreet Kaur <hkaur22@jaumebalmes.net>
2024-05-01 17:12:17 +0200

    migracio y taules done, templeates millorats

--- practica1/gestion-env/TIC_GestioPersonal/centre/models.py
+++ practica1/gestion-env/TIC_GestioPersonal/centre/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Rol(models.Model):
+    id = models.AutoField(primary_key=True)
+    nom = models.CharField(max_length=100)
+
+
+class Usuari(models.Model):
+    id = models.AutoField(primary_key=True)
+    nom = models.CharField(max_length=100)
+    cognom = models.CharField(max_length=100)
+    assignatures = models.CharField(max_length=200)
+    rol = models.ForeignKey(Rol, on_delete=models.CASCADE)
+
+
+
